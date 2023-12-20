8190edcf846fa07e6edaf23104415f7e2978af1b
Nithin Rajesh <rajeshnithin2305@csu.fullerton.edu>
2023-12-19 20:48:15 -0800

    Finished making the board, PHEW.

---
+++ script.js
@@ -0,0 +1,20 @@
+function populateBoard(size)
+{
+    let board = document.querySelector('canvas_container');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++)
+    {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
+
+function changeSize(size)
+{
+    populateBoard(size);
+}
+
+populateBoard(16);
